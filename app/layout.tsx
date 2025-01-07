a3884b033de300a3e20ad5d510657082c91030f9
Sviatoslav <dielok4@gmail.com>
2025-01-07 16:29:40 +0200

    Feat: connect redux in project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ReduxProvider from '@/app/store/Provider'
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-100`}
       >
-        {children}
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
